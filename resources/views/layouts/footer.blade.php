6e2093396a33d5c19378676391a88a1e73953088
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2022-09-30 12:52:01 -0700

    update9

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Footer opened -->
 	<div class="main-footer ht-40">
 		<div class="container-fluid pd-t-0-f ht-100p">
-			<span>Copyright © 2020 <a href="#">Valex</a>. Designed by <a href="https://www.spruko.com/">Spruko</a> All rights reserved.</span>
+			<span>Copyright © {{ date('Y') }} <a href="https://github.com/Eng-Hasan-Hajjar/">Eng-Hasan-Hajjar</a>. Designed by <a href="https://github.com/Eng-Hasan-Hajjar/">Eng-Hasan-Hajjar</a> All rights reserved.</span>
 		</div>
 	</div>
 <!-- Footer closed -->
--- resources/views/layouts/main-header.blade.php
+++ resources/views/layouts/main-header.blade.php
@@ -28,13 +28,24 @@
                         <a href="#" class="d-flex  nav-item nav-link pl-0 country-flag1" data-toggle="dropdown"
                             aria-expanded="false">
                             <span class="avatar country-Flag mr-0 align-self-center bg-transparent"><img
-                                    src="{{ URL::asset('assets/img/flags/us_flag.jpg') }}" alt="img"></span>
+                                    src="{{ URL::asset('assets/img/flags/sy-flag.jpg') }}" alt="img"></span>
                             <div class="my-auto">
-                                <strong class="mr-2 ml-2 my-auto">English</strong>
+                                <strong class="mr-2 ml-2 my-auto">Arabic</strong>
                             </div>
                         </a>
+
                         <div class="dropdown-menu dropdown-menu-left dropdown-menu-arrow" x-placement="bottom-end">
+
+
                             <a href="#" class="dropdown-item d-flex ">
+                                <span class="avatar  ml-3 align-self-center bg-transparent"><img
+                                        src="{{ URL::asset('assets/img/flags/us_flag.jpg') }}" alt="img"></span>
+                                <div class="d-flex">
+                                    <span class="mt-2">English</span>
+                                </div>
+                            </a>
+
+                        <a href="#" class="dropdown-item d-flex ">
                                 <span class="avatar  ml-3 align-self-center bg-transparent"><img
                                         src="{{ URL::asset('assets/img/flags/french_flag.jpg') }}" alt="img"></span>
                                 <div class="d-flex">
